425de72f31ad881fe8a6a939bacf5a5d25d49b04
unknown <sahar.booriaei7@google.com>
2023-08-04 10:45:50 +0430

    add render function in main.js

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+const root = document.getElementById("root");
+
+function render(plantslist) {
+    const template = plantslist.map((plant, index) => {
+      const { id, name, Light, Water, price, imgSrc } = plant;
+      return `
+        <section class="product" >
+            <img src="./CSS/Images/${imgSrc}">
+                <div class="card">
+                     <h3> Name: ${name}</h3>
+                     <h4> Light: ${Light}</h4>
+                     <h4> Water: ${Water}</h4>
+                     <h4> Price: ${price}</h4>
+                     <button onclick="addToCart()">SHOP NOW</button>
+                </div>  
+        </section>
+        `;
+    }).join("");
+    root.innerHTML += template;
+  }
+  
+  
+window.addEventListener("load", () => {
+    render(plants);});
+
+
+
